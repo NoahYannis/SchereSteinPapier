65c2f216d3719ffb761f1a9f6c1888caf75fcb87
NoahYannis <noah.guderjahn@gmx.de>
2022-04-09 14:49:16 +0200

    Projektdateien hinzufügen.

---
+++ Schere Stein Papier/SchereSteinPapier.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace Schere_Stein_Papier
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblSSP = new System.Windows.Forms.Label();
+            this.pbSpieler = new System.Windows.Forms.PictureBox();
+            this.pbGegner = new System.Windows.Forms.PictureBox();
+            this.lblSpieler = new System.Windows.Forms.Label();
+            this.lblGegner = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSpieler)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGegner)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSSP
+            // 
+            this.lblSSP.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSSP.ForeColor = System.Drawing.Color.Black;
+            this.lblSSP.Location = new System.Drawing.Point(45, 25);
+            this.lblSSP.Name = "lblSSP";
+            this.lblSSP.Size = new System.Drawing.Size(870, 118);
+            this.lblSSP.TabIndex = 0;
+            this.lblSSP.Text = "Schere, Stein, Papier!";
+            this.lblSSP.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pbSpieler
+            // 
+            this.pbSpieler.Image = global::Schere_Stein_Papier.Properties.Resources.SchereSteinPapier_Stein;
+            this.pbSpieler.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbSpieler.InitialImage")));
+            this.pbSpieler.Location = new System.Drawing.Point(115, 159);
+            this.pbSpieler.Name = "pbSpieler";
+            this.pbSpieler.Size = new System.Drawing.Size(351, 204);
+            this.pbSpieler.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSpieler.TabIndex = 1;
+            this.pbSpieler.TabStop = false;
+            // 
+            // pbGegner
+            // 
+            this.pbGegner.Image = global::Schere_Stein_Papier.Properties.Resources.SchereSteinPapier_Schere;
+            this.pbGegner.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbGegner.InitialImage")));
+            this.pbGegner.Location = new System.Drawing.Point(516, 161);
+            this.pbGegner.Name = "pbGegner";
+            this.pbGegner.Size = new System.Drawing.Size(359, 201);
+            this.pbGegner.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGegner.TabIndex = 2;
+            this.pbGegner.TabStop = false;
+            // 
+            // lblSpieler
+            // 
+            this.lblSpieler.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSpieler.ForeColor = System.Drawing.Color.Black;
+            this.lblSpieler.Location = new System.Drawing.Point(205, 391);
+            this.lblSpieler.Name = "lblSpieler";
+            this.lblSpieler.Size = new System.Drawing.Size(137, 52);
+            this.lblSpieler.TabIndex = 3;
+            this.lblSpieler.Text = "Spieler";
+            this.lblSpieler.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblGegner
+            // 
+            this.lblGegner.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblGegner.ForeColor = System.Drawing.Color.Black;
+            this.lblGegner.Location = new System.Drawing.Point(625, 394);
+            this.lblGegner.Name = "lblGegner";
+            this.lblGegner.Size = new System.Drawing.Size(166, 49);
+            this.lblGegner.TabIndex = 4;
+            this.lblGegner.Text = "Gegner";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.Location = new System.Drawing.Point(402, 460);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(166, 60);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(976, 560);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblGegner);
+            this.Controls.Add(this.lblSpieler);
+            this.Controls.Add(this.pbGegner);
+            this.Controls.Add(this.pbSpieler);
+            this.Controls.Add(this.lblSSP);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "SchereSteinPapier";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSpieler)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGegner)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSSP;
+        private System.Windows.Forms.PictureBox pbSpieler;
+        private System.Windows.Forms.PictureBox pbGegner;
+        private System.Windows.Forms.Label lblSpieler;
+        private System.Windows.Forms.Label lblGegner;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Schere Stein Papier/SchereSteinPapier.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Schere_Stein_Papier
+{
+    public partial class Form1 : Form
+    {
+
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int playerTurn = random.Next(1, 4);
+            int computerTurn = random.Next(1, 4);
+
+
+            switch (playerTurn)
+            {
+                case 1:
+                    pbSpieler.ImageLocation = @"C:\Users\User\Downloads\SchereSteinPapier.Stein.jpg";
+                    pbSpieler.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+
+                case 2:
+                    pbSpieler.ImageLocation = @"C:\Users\User\Downloads\SchereSteinPapier.Schere.jpg";
+                    pbSpieler.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+
+                case 3:
+                    pbSpieler.ImageLocation = @"C:\Users\User\Downloads\SchereSteinPapier.Papier.jpg";
+                    pbSpieler.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+
+                default:
+                    break;
+            }
+
+            switch (computerTurn)
+            {
+                case 1:
+                    pbGegner.ImageLocation = @"C:\Users\User\Downloads\SchereSteinPapier.Stein.jpg";
+                    pbGegner.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+
+                case 2:
+                    pbGegner.ImageLocation = @"C:\Users\User\Downloads\SchereSteinPapier.Schere.jpg";
+                    pbGegner.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+
+                case 3:
+                    pbGegner.ImageLocation = @"C:\Users\User\Downloads\SchereSteinPapier.Papier.jpg";
+                    pbGegner.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+
+                default:
+                    break;
+            }
+
+            BestimmeGewinner(playerTurn, computerTurn);
+        }
+
+        public void BestimmeGewinner(int playerScore, int computerScore)
+        {
+            if (playerScore == 1 && computerScore == 2)
+            {
+                lblSSP.Text = "Spieler gewinnt";
+            }
+            else if (playerScore == 1 && computerScore == 3)
+            {
+                lblSSP.Text = "Gegner gewinnt";
+            }
+            else if (playerScore == 1 && computerScore == 1)
+            {
+                lblSSP.Text = "Unentschieden";
+            }
+            else if (playerScore == 2 && computerScore == 1)
+            {
+                lblSSP.Text = "Gegner gewinnt";
+            }
+            else if (playerScore == 2 && computerScore == 3)
+            {
+                lblSSP.Text = "Spieler gewinnt";
+            }
+            else if (playerScore == 2 && computerScore == 2)
+            {
+                lblSSP.Text = "Unentschieden";
+            }
+            else if (playerScore == 3 && computerScore == 1)
+            {
+                lblSSP.Text = "Spieler gewinnt";
+            }
+            else if (playerScore == 3 && computerScore == 2)
+            {
+                lblSSP.Text = "Gegner gewinnt";
+            }
+            else if (playerScore == 3 && computerScore == 3)
+            {
+                lblSSP.Text = "Unentschieden";
+            }
+        }
+
+    }
+}
